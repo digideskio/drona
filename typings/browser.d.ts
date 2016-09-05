1b347f2216a4ab9e8bdc01beeb57879e5c17d5ef
James Macfie <james@macfie.co.nz>
2016-09-05 22:00:05 +1200

    Remove typings

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/react/index.d.ts" />
-/// <reference path="browser/ambient/redux/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
-/// <reference path="main/ambient/redux/index.d.ts" />
